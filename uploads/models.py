3eb0eb343c40d0635612659fe4ae11556cf1b853
mirokrastev <mirokrastev@protonmail.com>
2022-03-19 23:47:20 +0200

    move remaining space validation to model

--- uploads/models.py
+++ uploads/models.py
@@ -1,4 +1,8 @@
+import sys
+
 from django.db import models
+from django.core.exceptions import ValidationError
+from django.utils.translation import gettext_lazy as _
 
 from base.models import BaseModel
 from accounts.models import User
@@ -17,6 +21,11 @@ class FileUpload(BaseModel):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='uploads')
     file = models.FileField(upload_to=get_upload_path)
     date = models.DateTimeField(auto_now_add=True)
+
+    def clean(self):
+        remaining_space = self.user.remaining_space - sys.getsizeof(self.file.file)
+        if remaining_space < 0:
+            raise ValidationError(_('You do not have enough space'))
 
     def __str__(self):
         return str(self.file)
